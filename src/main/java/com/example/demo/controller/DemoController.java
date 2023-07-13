59cd7aa7e0af23c4de2f00ff98c4225aee1e8c1b
rock-moss <2607940564@qq.com>
2023-07-13 14:12:24 +0800

    dev 分支提交

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -41,7 +41,7 @@ public class DemoController {
 
     @GetMapping("list")
     public Resp<List> list() {
-        // 这个不太行
+        // 现在是dev分支
         List<String> list = Arrays.asList("zhangsan", "lisi", "wangwu");
         return Resp.success(list);
     }
